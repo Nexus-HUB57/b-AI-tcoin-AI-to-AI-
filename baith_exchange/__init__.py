8c2f21bd1540bb5efe0216183f582d98b2a6e1e7
Nexus-HUB57 <2.60985786e+08+Nexus-HUB57@users.noreply.github.com>
2026-09-23 14:04:45 +0000

    feat: integrate BAITHex preparation with Obscura evidence

--- baith_exchange/__init__.py
+++ baith_exchange/__init__.py
@@ -1,4 +1,8 @@
 from .service import BaithExchange, ExchangeConfig, ExchangeError
 from .hsm_adapter import BaithHsmMpcAdapter
+from .obscura import BaithObscuraCoordinator, ObscuraEvidence, ObscuraEvidenceError
 
-__all__ = ["BaithExchange", "ExchangeConfig", "ExchangeError", "BaithHsmMpcAdapter"]
+__all__ = [
+    "BaithExchange", "ExchangeConfig", "ExchangeError", "BaithHsmMpcAdapter",
+    "BaithObscuraCoordinator", "ObscuraEvidence", "ObscuraEvidenceError",
+]
